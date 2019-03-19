a20da66b1682db14528d8b8d29ea0db344933eeb
Charlie Hilton <charllie.hiltons@gmail.com>
2019-03-18 22:17:55 -0500

    it may all make sense

--- coolcouriers/src/App.js
+++ coolcouriers/src/App.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
+
 import './styles/css/app.css';
 //import Navbar from './components/navbar'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import Home from './Home';
+import Login from './Login';
+import Protected from './Protected';
+
+function onAuthRequired({history}) {
+  history.push('/login');
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Cool Hands Courier
-        </header>
-      </div>
+      <Router>
+        <Security issuer='https://dev-794098.okta.com/oauth2/default'
+                  client_id='{0oad3z61aNNdrnwyg356}'
+                  redirect_uri={window.location.origin + '/implicit/callback'}
+                  onAuthRequired={onAuthRequired} >
+          <Route path='/' exact={true} component={Home} />
+          <SecureRoute path='/protected' component={Protected} />
+          <Route path='/login' render={() => <Login baseUrl='https://dev-794098.okta.com' />} />
+          <Route path='/implicit/callback' component={ImplicitCallback} />
+        </Security>
+      </Router>
     );
   }
 }
