9e735592ba47ff27287046c30d77101842067795
System Administrator <root@Kaitlyns-MacBook-Pro.local>
2019-03-18 21:40:16 -0500

    staging

--- coolcouriers/src/components/navbar.js
+++ coolcouriers/src/components/navbar.js
@@ -1,22 +1,19 @@
 import React, { Component } from 'react';
-import './navbar.css';
+import '../styles/css/navbar.css';
 
 class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-        <Container>
 
           <Navbar bg="dark" variant="dark">
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
+            <Navbar className="mr-auto">
+              <Navbar.Link href="#home">Home</Navbar.Link>
+              <Navbar.Link href="#features">Features</Navbar.Link>
+              <Navbar.Link href="#pricing">Pricing</Navbar.Link>
+            </Navbar>
           </Navbar>
-
-        </Container>
       </div>
     );
   }
